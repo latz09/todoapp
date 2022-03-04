34cb85a670f27ed43be90ce167a849a9e78a53d1
latz09 <85132328+latz09@users.noreply.github.com>
2022-03-04 17:38:04 -0600

    main layout

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   content: ['./index.html'],
   theme: {
-    extend: {},
+    extend: {
+      flex: {
+        '1/4': '1 1 25%',
+        '1/2': '1 1 50%',
+      }  
+    },
   },
   plugins: [],
 }
